5b9141e4521515579974fbaefffb540b2e047e12
Karthi <jacksparrowd492@gmail.com>
2026-02-06 22:57:55 +0530

    Fix expected result in addition test

--- sample-app/src/test/java/com/example/CalculatorTest.java
+++ sample-app/src/test/java/com/example/CalculatorTest.java
@@ -8,6 +8,7 @@ public class CalculatorTest {
     @Test
     public void testAddition() {
         Calculator calc = new Calculator();
-        assertEquals(5, calc.add(2, 3));
+        assertEquals(6, calc.add(2, 3));
     }
 }
+
